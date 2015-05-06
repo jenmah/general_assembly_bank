947ee7660e9ac96c4824d2a864498100ac40ea65
Jennifer Mah <jen.mah@hotmail.com>
2015-05-06 21:21:46 +0100

    Have the deposit and withdrawal functions working through a click function and updating the display

--- atm.js
+++ atm.js
@@ -4,22 +4,9 @@ var balance1 = 0;
 var balance2 = 0;
 
 
-// logs the information that is typed into both input boxes
-//  $(document).ready(function() {
-//   $('#amount1').on('keyup', function(event) {
-//     amount1 = $(this).val();
-//     console.log(amount1);
-//   });
-//   $('#amount2').on('keyup', function(event) {
-//     amount2 = $(this).val();
-//     console.log(amount2);
-//   });
-
-
-
-
-// displays updating values when the deposit button is clicked
 $(document).ready(function() {
+
+// logs the input when the deposit button is clicked, and adds new deposit amounts to the chequing account
   $('#deposit1').on('click', function(event) {
     var deposit = parseInt($('#amount1').val());
     total1 = total1 + deposit;
@@ -28,37 +15,40 @@ $(document).ready(function() {
   $('#deposit2').on('click', function(event) {
     var deposit = parseInt($('#amount2').val());
     total2 = total2 + deposit;
-    $('#balance1').html('$' + total2);
+    $('#balance2').html('$' + total2);
   });
 
-  // $('#deposit2').on('click', function(event) {
-  //   $('#balance2').text('$' + amount2);
-  // });
-
+// logs the input when the withdraw button is clicked, and takes away the withdrawl amount from the chequing account
+ $('#withdraw1').on('click', function(event) {
+    var deposit = parseInt($('#amount1').val());
+    total1 = total1 - deposit;
+    $('#balance1').html('$' + total1);
+  }); 
+  $('#withdraw2').on('click', function(event) {
+    var deposit = parseInt($('#amount2').val());
+    total2 = total2 - deposit;
+    $('#balance2').html('$' + total2);
+  });
 
 })
 
 
 
+// attempt at trying to combine the two totals so that chequing can interact with the savings account
+var totalBalance = total1 + total2;
+
+
+if (total1 <= -1 && totalBalance >= amountBorrowed) {
+    var amountBorrowed = 
+    alert('I do not know how to make this change!');
+}
 
 
 
 
 
-// $(document).ready(function() {
-//   $('#deposit1').on('click', function(event) {
-//     $('#balance1').text('$' + amount1);
-//   });
-//   $('#deposit2').on('click', function(event) {
-//     $('#balance2').text('$' + amount2);
-//   });
-// })
 
 
-  // $('#deposit2').on('click', function(event) {
-  //   $('#balance2').text(parseInt($('#balance2').text()[1]) + parseInt(amount2));
-//   });
-// })
-
+// $('.account').css("border", "pink");
 
 
